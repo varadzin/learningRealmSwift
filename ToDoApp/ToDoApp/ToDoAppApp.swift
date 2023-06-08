93b806251609b2dbccdbf009364a59f86e76dce4
Frantisek Varadzin <varadzin@gmail.com>
2023-06-08 13:48:27 +0200

    setting Project

---
+++ ToDoApp/ToDoApp/ToDoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoAppApp.swift
+//  ToDoApp
+//
+//  Created by Frantisek Varadzin on 08.06.23.
+//
+
+import SwiftUI
+
+@main
+struct ToDoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
